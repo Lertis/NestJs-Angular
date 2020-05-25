0cbecb7fd8e20fbd2bb0a7138e2404552fc9ab8d
Yura Husak <yura.gusak7@gmail.com>
2020-05-25 14:56:05 +0300

    Added default config instead of null

--- angular-frontend/src/app/shared/services/redirect.service.ts
+++ angular-frontend/src/app/shared/services/redirect.service.ts
@@ -4,9 +4,18 @@ import { Router, NavigationExtras } from '@angular/router';
 @Injectable()
 export class RedirectService {
 
+	private readonly defaultNavigationExtras: NavigationExtras = {
+		relativeTo:  null,
+		queryParams: null,
+		queryParamsHandling: null,
+		preserveFragment: false,
+		skipLocationChange: false,
+		replaceUrl: false
+	};
+
 	constructor(private readonly router: Router) { }
 
 	public redirectTo(url: string, extras?: NavigationExtras) {
-		this.router.navigate([url], extras ? extras : null);
+		this.router.navigate([url], extras ? extras : this.defaultNavigationExtras);
 	}
 }
