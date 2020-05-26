908219dd1545accab66e5c11ba760c055954ed9a
Yura Husak <yura.gusak7@gmail.com>
2020-05-26 20:05:43 +0300

    Added prettier

--- angular-frontend/src/app/shared/components/confirmation-pop-up/confirmation-pop-up.component.ts
+++ angular-frontend/src/app/shared/components/confirmation-pop-up/confirmation-pop-up.component.ts
@@ -1,28 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-	selector: 'app-confirmation-pop-up',
-	templateUrl: './confirmation-pop-up.component.html',
-	styleUrls: ['./confirmation-pop-up.component.scss']
+  selector: 'app-confirmation-pop-up',
+  templateUrl: './confirmation-pop-up.component.html',
+  styleUrls: ['./confirmation-pop-up.component.scss'],
 })
-export class ConfirmationPopUpComponent implements OnInit {
+export class ConfirmationPopUpComponent {
+  public config = {
+    text: '',
+  };
 
-	public config = {
-		text: ''
-	};
+  constructor(private readonly dialogRef: MatDialogRef<ConfirmationPopUpComponent>) {}
 
-	constructor(private readonly dialogRef: MatDialogRef<ConfirmationPopUpComponent>) { }
+  public confirmYes() {
+    this.dialogRef.close(true);
+  }
 
-	ngOnInit(): void {
-	}
-
-	public confirmYes() {
-		this.dialogRef.close(true);
-	}
-
-	public confirmNo() {
-		this.dialogRef.close(false);
-	}
-
+  public confirmNo() {
+    this.dialogRef.close(false);
+  }
 }
