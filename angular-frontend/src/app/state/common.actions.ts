0ffbfdde13503df4917abd3c9aa57df360fa2d57
Yura Husak <yura.gusak7@gmail.com>
2020-05-29 18:11:45 +0300

    Remove unnecessary instance of service

--- angular-frontend/src/app/state/common.actions.ts
+++ angular-frontend/src/app/state/common.actions.ts
@@ -1,3 +1,12 @@
-import { createAction, props } from '@ngrx/store';
+import {createAction, props, Action} from '@ngrx/store';
 
-export const apiError = createAction('API.ERROR', props<{ err: any }>());
+export const apiError = createAction('API.ERROR', props<{err: any}>());
+export const actionRequest = createAction(
+	'ACTION.REQUEST',
+	props<{
+		url: string;
+		params: any;
+		resActions: (res) => Action[];
+		errorActions: (res) => Action[];
+	}>()
+);
