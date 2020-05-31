3b6b9f33649eecae148e92d79049318b4af787cc
Yura Husak <yura.gusak7@gmail.com>
2020-05-31 14:29:55 +0300

    Added AuthGuards

--- angular-frontend/src/app/modules/about/about-routing.ts
+++ angular-frontend/src/app/modules/about/about-routing.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AuthGuard } from '../../shared/guards/auth.guards';
 
-
-const routes: Routes = [
-	{ path: '', component: AboutComponent }
-];
+const routes: Routes = [{ path: '', component: AboutComponent, canActivate: [AuthGuard] }];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
-	exports: [RouterModule]
+	exports: [RouterModule],
 })
-export class AboutRoutingModule { }
+export class AboutRoutingModule {}
--- angular-frontend/src/app/modules/charts/charts-routing.module.ts
+++ angular-frontend/src/app/modules/charts/charts-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChartsWrapperComponent } from './components/charts-wrapper/charts-wrapper.component';
+import { AuthGuard } from '../../shared/guards/auth.guards';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: ChartsWrapperComponent,
+		canActivate: [AuthGuard],
 	},
 ];
 
