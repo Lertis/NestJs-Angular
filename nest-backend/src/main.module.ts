419d1d373be800a813485ff53fe1e26ef389aab6
Yura Husak <yura.gusak7@gmail.com>
2020-06-01 13:46:37 +0300

    Added Schedule cron module

--- nest-backend/src/main.module.ts
+++ nest-backend/src/main.module.ts
@@ -1,4 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ProductsModule } from './modules/products/products.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { AuthGuard } from './guards/auth.guard';
@@ -7,9 +8,18 @@ import { LoggerModule } from './modules/logger/logger.module';
 import { DynamicDumpModule } from './modules/dynamic/dynamic.dump.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { GridModule } from './modules/grid/grid.module';
+import { TasksModule } from './modules/cron/task.module';
 
 @Module({
-	imports: [ProductsModule, LoggerModule, DynamicDumpModule.forRoot(), AuthModule, GridModule],
+	imports: [
+		ProductsModule,
+		LoggerModule,
+		DynamicDumpModule.forRoot(),
+		AuthModule,
+		GridModule,
+		ScheduleModule.forRoot(),
+		TasksModule,
+	],
 	controllers: [],
 	providers: [AuthGuard, LoggingInterceptor],
 })
